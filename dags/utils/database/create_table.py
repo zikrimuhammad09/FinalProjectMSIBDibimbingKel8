6fa9803aa68d72da7e34eb66e014b883e8e7582f
gdearya2 <forcegini@gmail.com>
2023-11-29 22:39:30 +0700

    fix table

--- dags/utils/database/create_table.py
+++ dags/utils/database/create_table.py
@@ -6,15 +6,15 @@ def create_table():
     conn = database.connection.conn()
     cursor = conn.cursor()
     try:
-        # Buat tabel orders,coupons, customer, login_attempts, product_category, product, supplier, order_item
+        # Buat tabel orders, customers, coupons, login_attempt_history, product_categories, products, suppliers, order_items
         cursor.execute('CREATE TABLE IF NOT EXISTS orders (id INTEGER, customer_id INTEGER, status TEXT, created_at TIMESTAMP)')
-        cursor.execute('CREATE TABLE IF NOT EXISTS coupons (id INTEGER, order_id INTEGER, amount INTEGER, code TEXT)')
-        cursor.execute('CREATE TABLE IF NOT EXISTS customer (id INTEGER, name TEXT, email TEXT, address TEXT, phone TEXT)')
-        cursor.execute('CREATE TABLE IF NOT EXISTS login_attempts (id INTEGER, customer_id INTEGER, time TIMESTAMP, ip_address TEXT)')
-        cursor.execute('CREATE TABLE IF NOT EXISTS product_category (id INTEGER, name TEXT)')
-        cursor.execute('CREATE TABLE IF NOT EXISTS product (id INTEGER, name TEXT, price INTEGER, category_id INTEGER, supplier_id INTEGER)')
-        cursor.execute('CREATE TABLE IF NOT EXISTS supplier (id INTEGER, name TEXT, address TEXT, phone TEXT)')
-        cursor.execute('CREATE TABLE IF NOT EXISTS order_item (id INTEGER, order_id INTEGER, product_id INTEGER, quantity INTEGER, price INTEGER)')
+        cursor.execute('CREATE TABLE IF NOT EXISTS coupons (id INTEGER, discount_percent FLOAT)')
+        cursor.execute('CREATE TABLE IF NOT EXISTS customers (id INTEGER, first_name VARCHAR(80), last_name VARCHAR(80), address VARCHAR(255), gender VARCHAR(20), zip_code VARCHAR(20))')
+        cursor.execute('CREATE TABLE IF NOT EXISTS login_attempt_history (id INTEGER, customer_id INTEGER, login_successfull BOOLEAN, attempted_at TIMESTAMP)')
+        cursor.execute('CREATE TABLE IF NOT EXISTS product_categories (id INTEGER, name VARCHAR(100))')
+        cursor.execute('CREATE TABLE IF NOT EXISTS products (id INTEGER, name VARCHAR(100), price FLOAT, category_id INTEGER, supplier_id INTEGER)')
+        cursor.execute('CREATE TABLE IF NOT EXISTS suppliers (id INTEGER, name VARCHAR(100), country VARCHAR(100))')
+        cursor.execute('CREATE TABLE IF NOT EXISTS order_items (id INTEGER, order_id INTEGER, product_id INTEGER, amount INTEGER, coupon_id INTEGER)')
         conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print("Error: %s" % error)

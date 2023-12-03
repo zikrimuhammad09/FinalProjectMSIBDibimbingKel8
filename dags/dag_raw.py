49fdea6420ba3fb781857b19dd61d35a3bc2676b
gdearya2 <forcegini@gmail.com>
2023-12-03 21:54:07 +0700

    load task bukan load task 1

--- dags/dag_raw.py
+++ dags/dag_raw.py
@@ -91,11 +91,11 @@ transform_task = PythonOperator(
     dag             = dag2
 )
 
-load_task1 = PythonOperator(
+load_task = PythonOperator(
     task_id         = "load_data",
     python_callable = etl.load_data,
     provide_context = True,
     dag             = dag2
 )
 
-create_table >> [extract_order, extract_customer, extract_coupons, extract_login_attempts, extract_product_category, extract_product, extract_supplier, extract_order_item] >> transform_task >> load_task1
+create_table >> [extract_order, extract_customer, extract_coupons, extract_login_attempts, extract_product_category, extract_product, extract_supplier, extract_order_item] >> transform_task >> load_task

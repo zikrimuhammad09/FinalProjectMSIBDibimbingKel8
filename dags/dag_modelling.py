2f22d4542f67c27794f128abcb228e0a81cf473a
gdearya2 <forcegini@gmail.com>
2023-12-07 15:09:14 +0700

    update

--- dags/dag_modelling.py
+++ dags/dag_modelling.py
@@ -18,78 +18,78 @@ dag_modelling = DAG (
     schedule      = None
 )
 
-# create_schema = PythonOperator(
-#     task_id         = "create_schema",
-#     python_callable = data_modelling.create_schema,
-#     provide_context = True,
-#     dag             = dag_modelling
-# )
-# create_dim_coupons = PythonOperator(
-#     task_id         = "create_dim_coupons",
-#     python_callable = data_modelling.create_dim_coupons,
-#     provide_context = True,
-#     dag             = dag_modelling
-# )
-# create_dim_customers = PythonOperator(
-#     task_id         = "create_dim_customers",
-#     python_callable = data_modelling.create_dim_customers,
-#     provide_context = True,
-#     dag             = dag_modelling
-# )
-# create_dim_date = PythonOperator(
-#     task_id         = "create_dim_date",
-#     python_callable = data_modelling.create_dim_date,
-#     provide_context = True,
-#     dag             = dag_modelling
-# )
-# create_dim_orders = PythonOperator(
-#     task_id         = "create_dim_orders",
-#     python_callable = data_modelling.create_dim_orders,
-#     provide_context = True,
-#     dag             = dag_modelling
-# )
-# create_dim_products = PythonOperator(
-#     task_id         = "create_dim_products",
-#     python_callable = data_modelling.create_dim_products,
-#     provide_context = True,
-#     dag             = dag_modelling
-# )
-# create_product_categories = PythonOperator(
-#     task_id         = "create_product_categories",
-#     python_callable = data_modelling.create_product_categories,
-#     provide_context = True,
-#     dag             = dag_modelling
-# )
-# create_suppliers = PythonOperator(
-#     task_id         = "create_suppliers",
-#     python_callable = data_modelling.create_suppliers,
-#     provide_context = True,
-#     dag             = dag_modelling
-# )
-# create_fact_login_attempt_history = PythonOperator(
-#     task_id         = "create_fact_login_attempt_history",
-#     python_callable = data_modelling.create_fact_login_attempt_history,
-#     provide_context = True,
-#     dag             = dag_modelling
-# )
-# create_fact_sales = PythonOperator(
-#     task_id         = "create_fact_sales",
-#     python_callable = data_modelling.create_fact_sales,
-#     provide_context = True,
-#     dag             = dag_modelling
-# )
-# create_fact_customer_conversion_rates = PythonOperator(
-#     task_id         = "create_fact_customer_conversion_rates",
-#     python_callable = data_modelling.create_fact_customer_conversion_rates,
-#     provide_context = True,
-#     dag             = dag_modelling
-# )
-# create_fact_popular_products = PythonOperator(
-#     task_id         = "create_fact_popular_products",
-#     python_callable = data_modelling.create_fact_popular_products,
-#     provide_context = True,
-#     dag             = dag_modelling
-# )
+create_schema = PythonOperator(
+    task_id         = "create_schema",
+    python_callable = data_modelling.create_schema,
+    provide_context = True,
+    dag             = dag_modelling
+)
+create_dim_coupons = PythonOperator(
+    task_id         = "create_dim_coupons",
+    python_callable = data_modelling.create_dim_coupons,
+    provide_context = True,
+    dag             = dag_modelling
+)
+create_dim_customers = PythonOperator(
+    task_id         = "create_dim_customers",
+    python_callable = data_modelling.create_dim_customers,
+    provide_context = True,
+    dag             = dag_modelling
+)
+create_dim_date = PythonOperator(
+    task_id         = "create_dim_date",
+    python_callable = data_modelling.create_dim_date,
+    provide_context = True,
+    dag             = dag_modelling
+)
+create_dim_orders = PythonOperator(
+    task_id         = "create_dim_orders",
+    python_callable = data_modelling.create_dim_orders,
+    provide_context = True,
+    dag             = dag_modelling
+)
+create_dim_products = PythonOperator(
+    task_id         = "create_dim_products",
+    python_callable = data_modelling.create_dim_products,
+    provide_context = True,
+    dag             = dag_modelling
+)
+create_product_categories = PythonOperator(
+    task_id         = "create_product_categories",
+    python_callable = data_modelling.create_product_categories,
+    provide_context = True,
+    dag             = dag_modelling
+)
+create_suppliers = PythonOperator(
+    task_id         = "create_suppliers",
+    python_callable = data_modelling.create_suppliers,
+    provide_context = True,
+    dag             = dag_modelling
+)
+create_fact_login_attempt_history = PythonOperator(
+    task_id         = "create_fact_login_attempt_history",
+    python_callable = data_modelling.create_fact_login_attempt_history,
+    provide_context = True,
+    dag             = dag_modelling
+)
+create_fact_sales = PythonOperator(
+    task_id         = "create_fact_sales",
+    python_callable = data_modelling.create_fact_sales,
+    provide_context = True,
+    dag             = dag_modelling
+)
+create_fact_customer_conversion_rates = PythonOperator(
+    task_id         = "create_fact_customer_conversion_rates",
+    python_callable = data_modelling.create_fact_customer_conversion_rates,
+    provide_context = True,
+    dag             = dag_modelling
+)
+create_fact_popular_products = PythonOperator(
+    task_id         = "create_fact_popular_products",
+    python_callable = data_modelling.create_fact_popular_products,
+    provide_context = True,
+    dag             = dag_modelling
+)
 create_fact_daily_returning_customer_rates = PythonOperator(
     task_id         = "create_fact_daily_returning_customer_rates",
     python_callable = data_modelling.create_fact_daily_returning_customer_rates,
@@ -97,6 +97,4 @@ create_fact_daily_returning_customer_rates = PythonOperator(
     dag             = dag_modelling
 )
 
-# create_schema >> [create_dim_coupons >> create_dim_customers >> create_dim_date >> create_dim_orders >> create_product_categories >> create_suppliers >> create_dim_products  >> create_fact_login_attempt_history >> create_fact_sales >> create_fact_customer_conversion_rates >> create_fact_popular_products >> create_fact_daily_returning_customer_rates]
-
-create_fact_daily_returning_customer_rates
+create_schema >> [create_dim_coupons >> create_dim_customers >> create_dim_date >> create_dim_orders >> create_product_categories >> create_suppliers >> create_dim_products  >> create_fact_login_attempt_history >> create_fact_sales >> create_fact_customer_conversion_rates >> create_fact_popular_products >> create_fact_daily_returning_customer_rates]

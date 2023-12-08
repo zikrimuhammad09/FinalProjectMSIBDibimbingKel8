1eff89f2a03afa2380c22e62a3c751d2f2c9ada4
gdearya2 <forcegini@gmail.com>
2023-12-08 19:09:30 +0700

    update handling duplicate data customer and relations

--- dags/utils/etl/transform.py
+++ dags/utils/etl/transform.py
@@ -22,6 +22,40 @@ def remove_missing_values(df):
         # Hapus missing value
         df.dropna(inplace=True)
 
+def duplicate_data_customer(df_customer,df_order,df_login_attempts):
+    print('Proses handling duplicate data customer')
+    df_unique_customer = df_customer.drop_duplicates(keep='first')
+    # find df_customer duplicates but except first
+    df_duplicate_customer = df_customer[~df_customer.index.isin(df_unique_customer.index)]
+    for idx in df_unique_customer.index:
+        # find the row in df_customer that has the same each column in df_customer[idx] give new variable
+        matching_rows = df_duplicate_customer[df_duplicate_customer.eq(df_customer.loc[idx]).all(axis=1)]
+        # index matching_rows
+        matching_rows_index = matching_rows.index
+        # cari customer_id di df_order yang sama dengan matching_rows.index
+        matching_order = df_order[df_order['customer_id'].isin(matching_rows_index)]
+        # cari customer_id di df_login_attempts yang sama dengan matching_rows.index
+        matching_login_attempts = df_login_attempts[df_login_attempts['customer_id'].isin(matching_rows_index)]
+        # replace customer_id di df_order dengan idx
+        matching_order['customer_id'] = idx
+        # replace customer_id di df_login_attempts dengan idx
+        matching_login_attempts['customer_id'] = idx
+
+        # Update df_order with the modified values
+        df_order.loc[matching_order.index] = matching_order
+
+        # Update df_login_attempts with the modified values
+        df_login_attempts.loc[matching_login_attempts.index] = matching_login_attempts
+
+    df_customer = df_customer.drop_duplicates(keep='first')
+
+    print('Handling Duplikat data Customer selesai')
+    print(df_customer.head(20))
+    print(df_order.head(50))
+    print(df_login_attempts(50))
+    return df_customer, df_order, df_login_attempts
+
+
 def transform_data(**context):
     # Pull Data dari XCom
     df_order = context['ti'].xcom_pull(key='df_order')
@@ -32,7 +66,6 @@ def transform_data(**context):
     df_product = context['ti'].xcom_pull(key='df_product')
     df_supplier = context['ti'].xcom_pull(key='df_supplier')
     df_order_item = context['ti'].xcom_pull(key='df_order_item')
-    # df_zip_code = context['ti'].xcom_pull(key='df_zip_code')
 
     print("Transforming data...")
 
@@ -57,6 +90,9 @@ def transform_data(**context):
     for df in [df_order, df_customer, df_coupons, df_login_attempts, df_product_category, df_product, df_supplier, df_order_item]:
         remove_duplicates(df)
         remove_missing_values(df)
+
+    # handling duplicate data customer
+    df_customer, df_order, df_login_attempts = duplicate_data_customer(df_customer, df_order, df_login_attempts)
 
     # Push Data ke XCom
     context['ti'].xcom_push(key='df_order', value=df_order)
